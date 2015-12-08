d363459d96a6345b6a3a3342b73d149b0c9560f3
JOG-MRD <ortegagi@u.washington.edu>
2015-12-07 17:51:29 -0800

    initial commit

---
+++ run_tests.py
@@ -0,0 +1,48 @@
+"""
+Executes the CLAWPACK runs necessary for the comparisons of different Riemann
+solvers with different parameters.
+"""
+from trial import run
+
+def run_tests():
+    """For now, just run a single test"""
+    
+    #Control (full solver)
+    full = run.RiemannRun("full", 1)
+    full.setup_directory()
+    full.execute()
+    
+    #Roedeep (depth tolerance only)
+    depths = []
+    depths = [5.0, 10.0, 20.0, 40.0, 70.0, 100.0]
+    for depth in depths:
+        roedeep = run.RiemannRun("roedeep_{}".format(round(depth)), 2)
+        roedeep.roe_minimum_depth = depth
+        roedeep.setup_directory()
+        roedeep.execute()
+    
+    #RoeCone (phase plane cone)
+    params = [
+        (10.0, 0.25, 0.2),
+        (50.0, 0.50, 0.2),
+        (10.0, 0.50, 0.2),
+        (50.0, 0.25, 0.2),
+        (10.0, 0.25, 0.4)
+    ]
+    for i in range(len(params)):
+        param = params[i]
+        roecone = run.RiemannRun("roecone_{}".format(i), 3)
+        roecone.roe_minimum_depth = param[0]
+        roecone.roe_momentum_ratio = param[1]
+        roecone.roe_depth_fraction = param[2]
+        roecone.setup_directory()
+        roecone.execute()
+    
+
+def clear_runs():
+    """Delete all test runs (i.e. to save space); be very careful before doing this"""
+    pass
+
+
+if __name__ == "__main__":
+    run_tests()
---
+++ trial/run.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+
+class RiemannRun(object):
+    """
+    Object containing data necessary for a single Riemann trial
+    """
+    
+    def __init__(self, identifier, run_type):
+        """Set necessary parameters (default values) for the run"""
+        self.identifier = identifier #string identifying this run
+        self.run_type = run_type     #1, 2 or 3;
+        
+        if run_type == 1:
+            """Full Run"""
+            
+        elif run_type == 2:
+            """Depth Tolerance"""
+            self.roe_minimum_depth = 100.0
+        elif run_type == 3:
+            """Cone Condition"""
+            self.roe_minimum_depth = 10.0
+            self.roe_depth_fraction = 0.2
+            self.roe_momentum_ratio = 0.25
+        else:
+            """Run type not recognized"""
+            raise ValueError("Invalid run type: {}".format(run_type))
+        pass
+    
+    
+    def setup_directory(self):
+        """
+        Creates a run directory for this run, deleting one if it already exists,
+        and copies/generates the neccessary files, based on the run type.
+        """
+        import os, shutil, glob
+        
+        run_directory_path = "runs/run_{}".format(self.identifier)
+        
+        #clear the run directory, if it already exists, then make a new one
+        try:
+            shutil.rmtree(run_directory_path)
+        except OSError:
+            pass
+        os.mkdir(run_directory_path)
+        
+        #make directory for and copy source files for the run
+        source_path = os.path.join(run_directory_path, "src")
+        os.mkdir(source_path)
+        
+        #common fortran files
+        for common_source in glob.glob(r'src/*.f90'):
+            shutil.copy(common_source, source_path)
+        
+        #Riemann solver
+        if self.run_type == 2:
+            riemann_source = "src/riemann/roedeep/rpn2_geoclaw.f"
+        elif self.run_type == 3:
+            riemann_source = "src/riemann/roecone/rpn2_geoclaw.f"
+        else:
+            riemann_source = "src/riemann/full/rpn2_geoclaw.f"
+        shutil.copy(riemann_source, source_path)
+        
+        #parameter file
+        self.__generate_parameter_file(source_path)
+        
+        #copy run files
+        run_files = ["Makefile", "setplot.py", "setrun.py"]
+        for rf in run_files:
+            shutil.copy("run_files/{}".format(rf), run_directory_path)
+        
+        #copy existing output directory
+        output_path = os.path.join(run_directory_path, "_output")
+        os.mkdir(output_path)
+        for outdir_file in glob.glob(r'run_files/_output/*'):
+            shutil.copy(outdir_file, output_path)
+        
+        
+        #if all has gone well so far, set run directory strings
+        self.run_directory_path = run_directory_path
+        self.output_path = output_path
+    
+    
+    def execute(self):
+        """Command to do run"""
+        import os
+        old_directory = os.getcwd()
+        os.chdir(self.run_directory_path)
+        
+        #execute in a new process
+        os.system("make all >> console.out &")
+        
+        os.chdir(old_directory)
+        
+        
+    
+    def __generate_parameter_file(self, source_path):
+        """
+        Generates FORTRAN file to set parameters for the Riemann solver
+        """
+        import os
+        
+        #make code to set variables
+        front_matter = [
+            "subroutine hybrid_parameters()",
+            "use amr_module, only: roe_depth_frac, roe_min_depth, roe_mom_rat"
+        ]
+        middle_matter = []
+        end_matter = ["end subroutine"]
+        
+        #based on Riemann solver type, set parameters
+        if self.run_type == 2:
+            middle_matter.extend([
+                "roe_min_depth = {}d0".format(self.roe_minimum_depth)
+            ])
+        elif self.run_type == 3:
+            middle_matter.extend([
+                "roe_depth_frac = {}d0".format(self.roe_depth_fraction),
+                "roe_min_depth = {}d0".format(self.roe_minimum_depth),
+                "roe_mom_rat = {}d0".format(self.roe_momentum_ratio)
+            ])
+        
+        code = front_matter + middle_matter + end_matter
+        
+        #write code to file
+        param_path = os.path.join(source_path, "hybrid_parameters.f90")
+        with open(param_path, 'w') as param_file:
+            for line in code:
+                param_file.write(line + '\n')
+        
+
+if __name__ == "__main__":
+    import os
+    olddir =  os.getcwd()
+    print olddir
+    os.chdir("..")
+    print os.getcwd()
+    os.chdir(olddir)
+    print os.getcwd()
+
